7c4d674a2e9bfcb6b51ad07c6eccefc9838a7a1a
komuro-hiraku <komuro.hiraku@gmail.com>
2025-01-27 22:57:22 +0900

    - LDAのOpsCode分岐を追加

--- src/libs/cpu.rs
+++ src/libs/cpu.rs
@@ -21,6 +21,23 @@ impl CPU {
             self.program_counter += 1; // increment pc
 
             match opscode {
+                0xA9 => {   // LDA
+                    let param = program[self.program_counter as usize];
+                    self.program_counter += 1;
+                    self.register_a = param;
+
+                    if self.register_a == 0 {
+                        self.status = self.status | 0b0000_0010;
+                    } else {
+                        self.status = self.status & 0b1111_1101;
+                    }
+
+                    if self.register_a & 0b1000_0000 != 0 {
+                        self.status = self.status | 0b1000_0000;
+                    } else {
+                        self.status = self.status & 0b0111_1111;
+                    }
+                }
                 _ => todo!("not implemented")
             }
         }
